038869b15e81698c706fdb58277ff7e838a630e5
liammcdade <83595569+liammcdade@users.noreply.github.com>
2023-02-25 21:17:08 +0000

    yeah

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,9 @@
 sudo apt update -y
 sudo apt upgrade -y
+sudo apt install software-properties-common apt-transport-https
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt install snap -y
 sudo snap install --classic code -y
 sudo snap install vlc -y
