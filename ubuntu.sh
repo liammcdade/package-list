1838545cdda7b4c079fe5296d202094986c7f22a
liammcdade <83595569+liammcdade@users.noreply.github.com>
2023-02-25 21:18:12 +0000

    yeah

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,8 +4,8 @@ sudo apt install software-properties-common apt-transport-https
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt install snap -y
-sudo snap install --classic code -y
+sudo apt update
+sudo apt install code
 sudo snap install vlc -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
