5fa0d610eb7704d89e2390fdd68198e49ea56948
liammcdade <83595569+liammcdade@users.noreply.github.com>
2023-02-25 22:23:24 +0000

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,11 +1,8 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install software-properties-common apt-transport-https
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt update
-sudo apt install code
+wget https://code.visualstudio.com/docs/?dv=linux64_deb
+sudo apt install code_1.75.1-1675893397_amd64.deb
 sudo snap install vlc -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
